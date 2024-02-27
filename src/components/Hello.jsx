a4b8b6563467daad957395123cc6fd1fec297520
ClaireHowley <clairehowley2@gmail.com>
2024-02-27 11:44:36 +0100

    p5 playground, three react downloaded

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,4 +1,26 @@
+import { useRef, useEffect } from "react";
+import p5 from "p5";
+
 export default function Hello() {
+	const sketchRef = useRef();
+
+	useEffect(() => {
+		const sketch = new p5((p) => {
+			p.setup = () => {
+				p.createCanvas(400, 400);
+			};
+
+			p.draw = () => {
+				p.background(220);
+				p.ellipse(p.mouseX, p.mouseY, 50, 50);
+			};
+		}, sketchRef.current);
+
+		return () => {
+			sketch.remove();
+		};
+	}, []);
+
 	return (
 		<div className="h-lvh">
 			<svg className="text-9xl pt-40 pl-10 md:pt-15 md:pl-20">
@@ -40,6 +62,7 @@ export default function Hello() {
 					</tspan>
 				</text>
 			</svg>
+			<div ref={sketchRef}></div>
 		</div>
 	);
 }
