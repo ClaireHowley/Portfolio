30091c6da5b0444fd4c73fb6d322469f213e96e9
ClaireHowley <clairehowley2@gmail.com>
2024-03-19 14:46:03 +0100

    spinning moving sphere added, three js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,32 @@
-import { forwardRef } from "react";
+import { forwardRef, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 const Contact = forwardRef((props, ref) => {
+	function Sphere({ position, size, color }) {
+		const sphereRef = useRef();
+		useFrame((state, delta) => {
+			sphereRef.current.rotation.x += delta;
+			// sphereRef.current.rotation.y += delta;
+		});
+
+		return (
+			<mesh ref={sphereRef} position={position}>
+				<sphereGeometry args={size} />
+				<meshStandardMaterial color={color} />
+			</mesh>
+		);
+	}
+
 	return (
 		<div ref={ref} id="contact">
 			Contact
+			<Canvas shadowMap>
+				<directionalLight position={[0, 0, 2]} intensity={0.5} />
+				<ambientLight intensity={0.1} />
+				<OrbitControls />
+				<Sphere position={[0, 0, 0]} size={[2, 34, 34]} color={"blue"} />
+			</Canvas>
 		</div>
 	);
 });
